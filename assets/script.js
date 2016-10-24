f804737eece20a5f11934543029b15b706394415
haconglinh1990 <haconglinh1990@gmail.com>
2016-10-24 17:10:44 +0700

    2nd Commit

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+//Loading bar
+
+$(".ajax-call").loadingbar({
+  replaceURL: false, /* You can visibly change the URL of the browser to reflect the clicked links by toggling this to true. Default is false. May not work in old browsers. */
+    target: "#loadingbar-frame", /* The container's selector where you want the ajax result to appear. Default is #loadingbar-frame */
+    direction: "right", /* The direction where the the loading bar will progress. Default is right. */
+
+    /* Default Ajax Parameters.  */
+    async: true, 
+    complete: function(xhr, text) {},
+    cache: true,
+    error: function(xhr, text, e) {},
+    global: true,
+    headers: {},
+    statusCode: {},
+    success: function(data, text, xhr) {},
+    dataType: "html",
+    done: function(data) {}
+});
